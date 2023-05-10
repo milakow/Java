07798074720acdf3ffc071aea8d93d85337e588e
milakow <kamila.kowalik97@gmail.com>
2023-05-10 19:53:40 +0200

    Sorter exercise2 commit

--- src/main/java/advancedDevelopment/task2/Sorter.java
+++ src/main/java/advancedDevelopment/task2/Sorter.java
@@ -1,5 +1,8 @@
 package advancedDevelopment.task2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 /**
  * Create a method that takes a list of strings as a parameter,
@@ -7,7 +10,9 @@ import java.util.List;
  */
 public class Sorter {
     public static void main(String[] args) {
-
+        Comparator<String> comparator = (s1, s2) -> s2.compareToIgnoreCase(s1);
+        List<String> list = new ArrayList<String>(List.of("A", "B", "C", "a", "b", "c"));
+        Collections.sort(list, comparator);
     }
 
     public static List<String> sortDescendingWithoutLetterCase(List<String> strings) {
